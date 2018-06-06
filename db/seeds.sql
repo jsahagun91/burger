9b3672b05ec7601552ac098945c6be26516b7ba1
Jose Sahagun <JoseSahagun@Joses-MacBook-Pro-2.local>
2018-06-05 18:32:44 -0700

    hw

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double-Double", boolean);
+INSERT INTO burgers (burger_name, devoured) VALUES ("3 x 3", boolean);
+INSERT INTO burgers (burger_name, devoured) VALUES ("4 x 4", boolean);
